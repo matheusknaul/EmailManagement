9567a14472710bf854ad855146dc7466bd5e3918
matheusknaul <contatomatheush0909@gmail.com>
2025-06-05 23:13:39 -0300

    feat(email): created email CRUD.

--- EmailManagement/Controllers/EmailsController.cs
+++ EmailManagement/Controllers/EmailsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using EmailManagement.Data;
 using EmailManagement.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmailManagement.Controllers
 {
@@ -16,17 +18,60 @@ namespace EmailManagement.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get() {
-            var emails = _context.Emails.ToList();
-            return Ok(emails);
+        public async Task<ActionResult<IEnumerable<Email>>> GetAll() {
+            return await _context.Emails.ToListAsync();
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Email>> GetById(int id)
+        {
+            var email = await _context.Emails.FindAsync(id);
+            if (email == null)
+                return NotFound();
+
+            return email;
+        }
+
+
         [HttpPost]
-        public IActionResult Post([FromBody] Email email) 
+        public async Task<ActionResult<Email>> Create(Email email)
         {
             _context.Emails.Add(email);
-            _context.SaveChanges();
-            return CreatedAtAction(nameof(Get), new { id = email.Id}, email);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = email.Id}, email);
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Email email)
+        {
+            if (id != email.Id)
+                return BadRequest();
+            _context.Entry(email).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Emails.Any(e=> e.Id == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var email = await _context.Emails.FindAsync(id);
+            if (email == null)
+                return NotFound();
+            _context.Emails.Remove(email);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
     }
 }
