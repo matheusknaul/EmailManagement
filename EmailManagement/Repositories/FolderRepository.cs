a50dc2392ba6ffa4cc97772df106e6dbf26c76f3
matheusknaul <contatomatheush0909@gmail.com>
2025-06-06 23:55:36 -0300

    feat: created FolderRepository and IFolderRepository

---
+++ EmailManagement/Repositories/FolderRepository.cs
@@ -0,0 +1,28 @@
+﻿using EmailManagement.Models;
+using EmailManagement.Repositories.Interfaces;
+
+namespace EmailManagement.Repositories
+{
+    public class FolderRepository : IFolderRepository
+    {
+        public Task<bool> CreateFolderAsync(Folder folder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteFolderAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Folder>> GetAllFoldersAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Folder> GetFolderByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EmailManagement/Repositories/Interfaces/IFolderRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmailManagement.Models;
+
+namespace EmailManagement.Repositories.Interfaces
+{
+    public interface IFolderRepository
+    {
+        Task<List<Folder>> GetAllFoldersAsync(int userId);
+        Task<Folder> GetFolderByIdAsync(int id);
+        Task<bool> CreateFolderAsync(Folder folder);
+        Task<bool> DeleteFolderAsync(int id);
+    }
+
+}
