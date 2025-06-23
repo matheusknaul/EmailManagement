fe083654b2ce88907bda1129d7f33ccf77fd5c12
matheusknaul <contatomatheush0909@gmail.com>
2025-06-23 02:53:30 -0300

    feat: Create method CreateDefaultFolders in UserService

--- EmailManagement/Services/UserService.cs
+++ EmailManagement/Services/UserService.cs
@@ -1,6 +1,22 @@
-﻿namespace EmailManagement.Services
+﻿using EmailManagement.Models;
+
+namespace EmailManagement.Services
 {
     public class UserService
     {
+
+        public void CreateDefaultFolders(int userId)
+        {
+            var folders = new List<Folder>
+            {
+                new Folder { UserId = userId, Name = "Inbox", isSystem = true },
+                new Folder { UserId = userId, Name = "Sent", isSystem = true },
+                new Folder { UserId = userId, Name = "Drafts", isSystem = true },
+                new Folder { UserId = userId, Name = "Spam", isSystem = true },
+                new Folder { UserId = userId, Name = "Trash", isSystem = true }
+            };
+
+        }
+
     }
 }
