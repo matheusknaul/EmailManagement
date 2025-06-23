8c8979721b371324ab80947ffd837c9cca2534fe
matheusknaul <contatomatheush0909@gmail.com>
2025-06-23 02:55:53 -0300

    feat: Create method GetEmailsBySearch (need implement logic and test)

--- EmailManagement/Services/EmailService.cs
+++ EmailManagement/Services/EmailService.cs
@@ -1,6 +1,17 @@
-﻿namespace EmailManagement.Services
+﻿using EmailManagement.Models;
+
+namespace EmailManagement.Services
 {
     public class EmailService
     {
+
+        public List<Email> GetEmailsBySearch()
+        {
+
+            //Adicionar lógica de busca de emails aqui
+
+            return new List<Email>();
+
+        }
     }
 }
