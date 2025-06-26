abcae03f9513ac1a161a969d9344d9822374352d
matheusknaul <contatomatheush0909@gmail.com>
2025-06-26 01:26:56 -0300

    feat: Created ScoredResultDTO Linked List with 3 paramethers (dont finished)

--- EmailManagement.Tests/Services/EmailServiceTest.cs
+++ EmailManagement.Tests/Services/EmailServiceTest.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EmailManagement.Services;
+using MySqlX.XDevAPI.Common;
 
 namespace EmailManagement.Tests.Services
 {
-    internal class EmailServiceTest
+    public class EmailServiceTest
     {
+        [Fact]
+        public void GetEmailsBySearchTest()
+        {
+            var query = "teste relatório azul";
+            var esperado = new string[] { "teste", "relatório", "azul" };
+            var emailService = new EmailService();
+
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            var result = emailService.GetEmailsBySearch(query);
+            stopwatch.Stop();
+
+            System.Console.WriteLine($"Tempo de execução: {stopwatch.ElapsedMilliseconds} ms");
+
+            Assert.Equal(esperado, result);
+
+        }
     }
+    
 }
---
+++ EmailManagement/DTOS/ScoredResultDTO.cs
@@ -0,0 +1,36 @@
+﻿namespace EmailManagement.DTOS
+{
+    public class NodeScoredResult<TKey, TValue, TWeigth>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+        public TWeigth Weight { get; set; }
+        public NodeScoredResult<TKey, TValue, TWeigth> Next { get; set; }
+
+        public NodeScoredResult(TKey key, TValue value, TWeigth weigth)
+        {
+            Key = key;
+            Value = value;
+            Weight = weigth;
+            Next = null;
+        }
+    }
+
+    public class ScoredResultDTO<TKey, TValue, TWeigth>
+    {
+        private NodeScoredResult<TKey, TValue, TWeigth> head;
+
+        public ScoredResultDTO()
+        {
+            head = null;
+        }
+
+        public void AddFirst(TKey key, TValue value, TWeigth weigth) 
+        {
+            NodeScoredResult<TKey,TValue,TWeigth> newNodeScored = new NodeScoredResult<TKey, TValue, TWeigth>(key, value, weigth);
+            newNodeScored.Next = head;
+            head = newNodeScored;
+        }
+
+    }
+}
